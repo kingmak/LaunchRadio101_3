cd3e01a65cee3fe39e908a9c601a411257285fb0
King Mak <kingmak@hotmail.com>
2015-05-02 05:12:22 -0500

    Update Radio101_3.py

--- Radio101_3.py
+++ Radio101_3.py
@@ -1,43 +1,51 @@
-import os, re, urllib2, subprocess, threading
-
-print 'You Need Windows 7'
-print 'You Need VLC Media Player\n'
-raw_input('if you have the above press enter else exit the program')
-
-regex = r'(?<=shoutcast_stream":").[^"]*'
-site = 'http://www.iheart.com/live/1013-kdwb-1201/'
-data = urllib2.urlopen(site).read()
-radioLink = re.search(regex, data).group()
+import os, re, sys, urllib2, platform, subprocess, threading
 
 try:
-    os.chdir('C:\Program Files (x86)\VideoLAN\VLC')
+    regex = r'(?<=shoutcast_stream":").[^"]*'
+    site = 'http://www.iheart.com/live/1013-kdwb-1201/'
+    data = urllib2.urlopen(site).read()
+    radioLink = re.search(regex, data).group()
 
-    # complicated bad code, i coud find no other way
-    def callVLC():
-        subprocess.call(["vlc", "-vvv", radioLink])
+except Exception, linkError:
+    print str(linkError)
+    sys.exit('')
 
-    VLCstart = threading.Thread(target = callVLC)
-    class MyThread(threading.Thread):
-        def run(self):
-            VLCstart.start()
+os = platform.system()
+if (os == 'Linux'):
+    sys.exit(radioLink)
 
-    thread = MyThread()
-    thread.daemon = True
-    thread.start()
+elif (os == 'Windows'):
 
-    #very bad
-    os.system('TASKKILL /F /IM python.exe')
-except Exception, e:
-    print str(e)
-    raw_input('')
+    try:
+        os.chdir('C:\Program Files (x86)\VideoLAN\VLC')
+        
+    except Exception, noVlc:
+        sys.exit(radioLink)
+
+    try:
+        # complicated bad code, i coud find no other way
+        def callVLC():
+            subprocess.call(["vlc", "-vvv", radioLink])
+
+        VLCstart = threading.Thread(target = callVLC)
+        class MyThread(threading.Thread):
+            def run(self):
+                VLCstart.start()
+
+        thread = MyThread()
+        thread.daemon = True
+        thread.start()
+
+        #very bad
+        os.system('TASKKILL /F /IM python.exe')
+    except Exception, e:
+        print str(e)
+        raw_input('')
+else:
+    sys.exit('OS is not Linux or Windows, but here is the link:\n' + radioLink)
 
 '''
-if you turn it into an exe via py2exe then use:
-os.system('TASKKILL /F /IM NAME_OF_UR_EXE.exe')
-
-windows media player can also be used, but for now code only for VLC, you can change if you want
-
+windows media player can also be used (You will have to implement yourself)
 location: "C:\Program Files (x86)\Windows Media Player"
-exeName = wmplayer.exe
 launchCommand = 'wmplayer.exe "' + radioLink +'"'
 '''
